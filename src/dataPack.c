223a18e5692f2fa6061a54639f98caf014b50483
Angus Logan <awjlogan@gmail.com>
2026-04-29 21:36:05 +0100

    [Data pack] Only print up to maximum number of OneWire devices.
      - If external max was 1, slots 1-3 were still being printed.

--- src/dataPack.c
+++ src/dataPack.c
@@ -167,7 +167,7 @@ size_t dataPackSerial(const EmonTHDataset_t *restrict pData,
   strn.n += strnCat(&strn, &baseStr[STR_PERIOD]);
   strn.n += strnCatInt(&strn, tempInt % 10);
 
-  for (int i = 0; i < TEMP_MAX_ONEWIRE; i++) {
+  for (int i = 0; i < (int)pData->numExtMax; i++) {
     /* Only include sensors that have been found */
     if (pData->tempExternal[i] != 4800) {
       tempInt = pData->tempExternal[i] * 62500; /* micro-degrees */
